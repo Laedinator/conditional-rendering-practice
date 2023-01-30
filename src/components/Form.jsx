811a35e87c8ba502a94f8e36197eb356446cb725
Laedinator <laedinator@gmail.com>
2023-01-30 20:44:58 +0100

    initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Input from "./Input";
+
+function Form(props) {
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      {!props.userIsRegistered && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.userIsRegistered ? "Login" : "Register"}</button>
+    </form>
+  );
+}
+
+export default Form;
